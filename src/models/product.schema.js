2e6bb7c0610751dcd9de50921562f7632da08840
VarunKumarGampa <71691126+VarunKumarGampa@users.noreply.github.com>
2023-05-07 07:15:45 +0000

    product schema

---
+++ src/models/product.schema.js
@@ -0,0 +1,40 @@
+import mongoose, { Collection } from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : ["true","Please provide the product name"],
+        trim:true,
+        maxLength: [120, "Product name should not be max than 120 chars"]
+    },
+    price :{
+        type: Number,
+        required : ["true","product name should not be max than 5 chars"]
+    },
+    description:{
+            type: String
+    },
+    photo:[
+        {
+            secure_url : {
+               type: String,
+               required : ["true"]
+            }
+
+        }
+    ],
+    stock:{
+        type:Number,
+        default : 0,
+    },
+    sold:{
+        type:Number,
+        default : 0,
+    },
+    collectoionID : {
+            ref Collection
+    }
+
+},{timestamps:true})
+
+export default mongoose.model("Product", productSchema)
